e2abdfbe0ae2e0845d2a5f61892d2cd631712e59
michelangelo truffarelli <michelangelo.truffarelli@outlook.com>
2020-10-23 22:55:30 +0200

    fine app

---
+++ ClassLampadina/Lampadina.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLampadina
+{
+    public class Lampadina
+    {
+        private int maxClick;
+        private String stato;
+        private int nclick;
+
+        public Lampadina(string s, int m)
+        {
+            maxClick = m;
+            stato = s;
+        }
+
+        public string Click()
+        {
+            nclick++;
+            if (nclick > maxClick)
+            {
+                stato = "rotta";
+            }
+            else if (stato == "accesa")
+            {
+                stato = "spenta";
+            }
+            else if (stato == "spenta")
+            {
+                stato = "accesa";
+            }
+            return stato;
+        }
+
+        public string Descrizione()
+        {
+            return stato;
+        }
+    }
+}
---
+++ LampadinaTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLampadina;
+
+namespace LampadinaTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampadina lp1 = new Lampadina("spenta", 5);
+            Console.WriteLine("Quanti click vuoi fare? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(lp1.Click());
+            }
+        }
+    }
+}
